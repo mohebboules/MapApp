36dc0cb893e58d8b06455310572099265bab59a7
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-06 23:42:57 +0200

    setting up the map using MapKit, updating the LocationsViewModel (iOS 17- method)

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -4,13 +4,35 @@
 //
 //  Created by Moheb Boules on 06/12/2024.
 //
-
+import SwiftUI
 import Foundation
+import MapKit
 
 class LocationsViewModel: ObservableObject{
+    // All loaded locations
     @Published var locations: [Location]
+    // current location on Map
+    @Published var mapLocation: Location
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion(){
+        didSet{
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
     init(){
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        
+        self.updateMapRegion(location: locations.first!)
+    }
+    private func updateMapRegion(location: Location){
+        withAnimation(.easeInOut){
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
     }
 }
