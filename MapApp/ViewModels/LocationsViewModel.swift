4f5b4aa1e32f0266833f7d696a0af7a98cd2cb16
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-07 00:05:13 +0200

    Fixing the bug caused by the wrong position of the clauses updating mapRegion automatically, provide a default value insteand of force unwrapping

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -12,21 +12,38 @@ class LocationsViewModel: ObservableObject{
     // All loaded locations
     @Published var locations: [Location]
     // current location on Map
-    @Published var mapLocation: Location
-    
-    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion(){
+    @Published var mapLocation: Location {
         didSet{
             updateMapRegion(location: mapLocation)
         }
     }
+    
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init(){
         let locations = LocationsDataService.locations
         self.locations = locations
-        self.mapLocation = locations.first!
-        
-        self.updateMapRegion(location: locations.first!)
+
+        guard let firstLocation = locations.first else {
+            let defaultLocation = Location(
+                name: "Default",
+                cityName: "Default",
+                coordinates: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+                description: "The Colosseum is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ever built, and is still the largest standing amphitheatre in the world today, despite its age.",
+                imageNames: [
+                   
+                ],
+                link: "https://en.wikipedia.org/wiki/"
+            )
+            
+            self.mapLocation = defaultLocation
+            self.updateMapRegion(location: defaultLocation)
+            print("Warning: LocationsDataService.locations is empty.")
+            return
+        }
+        self.mapLocation = firstLocation
+        self.updateMapRegion(location: firstLocation)
     }
     private func updateMapRegion(location: Location){
         withAnimation(.easeInOut){
