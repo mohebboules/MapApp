31b110ead1f28be57425d44bbe3fd99428b52898
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-06 22:28:18 +0200

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Moheb Boules on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
