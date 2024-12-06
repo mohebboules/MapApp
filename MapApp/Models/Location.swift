05530a78e0e5576358489c5f067dbd57952d2b48
Moheb Boules Guirges <mohebboules@gmail.com>
2024-12-06 22:52:46 +0200

    Location Model setup

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Moheb Boules on 06/12/2024.
+//
+import Foundation
+import MapKit
+
+struct location{
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
